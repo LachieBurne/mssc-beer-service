5d757e7f707489bc57433f15194311d4a49e9e60
LachieBurne <lachlanburne072899@gmail.com>
2025-02-05 13:47:31 +1100

    feat: add bootstrap

--- src/main/java/burne/springframework/mssc_beer_service/repositories/BeerRepository.java
+++ src/main/java/burne/springframework/mssc_beer_service/repositories/BeerRepository.java
@@ -1,9 +1,9 @@
 package burne.springframework.mssc_beer_service.repositories;
 
 import burne.springframework.mssc_beer_service.domain.Beer;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends CrudRepository<Beer, UUID> {
 }
