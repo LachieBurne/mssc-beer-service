a2e5b6b4f1233c0bd1eccd086b40bfbb9ca5614e
LachieBurne <lachlanburne072899@gmail.com>
2025-02-05 14:40:25 +1100

    feat: add controller advice and error handling

---
+++ src/main/java/burne/springframework/mssc_beer_service/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,24 @@
+package burne.springframework.mssc_beer_service.web.controller;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
+        List<String> errorsList = new ArrayList<>(e.getConstraintViolations().size());
+
+        e.getConstraintViolations().forEach(error -> errorsList.add(error.toString()));
+
+        return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
+    }
+
+}
