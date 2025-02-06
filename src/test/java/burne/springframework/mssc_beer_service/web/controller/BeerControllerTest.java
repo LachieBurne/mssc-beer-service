056b92f79d45bd0cab0a2e42385a5978180a1007
LachieBurne <lachlanburne072899@gmail.com>
2025-02-06 14:43:02 +1100

    feat: update tests

--- src/test/java/burne/springframework/mssc_beer_service/web/controller/BeerControllerTest.java
+++ src/test/java/burne/springframework/mssc_beer_service/web/controller/BeerControllerTest.java
@@ -1,5 +1,7 @@
 package burne.springframework.mssc_beer_service.web.controller;
 
+import burne.springframework.mssc_beer_service.domain.Beer;
+import burne.springframework.mssc_beer_service.repositories.BeerRepository;
 import burne.springframework.mssc_beer_service.web.model.BeerDto;
 import burne.springframework.mssc_beer_service.web.model.BeerStyleEnum;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -7,17 +9,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "burne.springframework.mssc_beer_service.web.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -26,8 +34,13 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockitoBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeeryById() throws Exception {
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
